63b51a37688217e9b08c4ef59c9cc0e515b19dee
Sara Santos <saramcssantos@gmail.com>
2025-06-20 16:30:10 -0300

    Importanto arquivo de lógica

--- menu.py
+++ menu.py
@@ -1,13 +1,13 @@
-import aluga_filmes
+from main import consulta_filmes,aluga_filme, devolve_filme
 
-opcao = int(input('Digite uma opção 1 para consultar filmes ou 2 para alugar filme:  '))
+opcao = int(input('Digite:\n 1 - Consultar filmes\n 2 - Alugar filme\n 3 - Devolver filme\n Escolha: '))
 
 match opcao:
     case 1:
-        aluga_filmes.consulta_filmes()
+        consulta_filmes()
     case 2:
         aluga = int(input('Informe o filme que deseja alugar: '))
-        aluga_filmes.aluga_filme(aluga)
+        print(aluga_filme(aluga))
     case 3:
         devolve = int(input('Informe o filme que deseja devolver: '))
-        aluga_filmes.devolve_filme(devolve)
+        print(devolve_filme(devolve))
