4dc588e464ed47fa93aafef1531f2d884fc4c747
Sara Santos <saramcssantos@gmail.com>
2025-06-20 13:06:38 -0300

    Adiciona arquivo de interação com usuário

---
+++ menu.py
@@ -0,0 +1,13 @@
+import aluga_filmes
+
+opcao = int(input('Digite uma opção 1 para consultar filmes ou 2 para alugar filme:  '))
+
+match opcao:
+    case 1:
+        aluga_filmes.consulta_filmes()
+    case 2:
+        aluga = int(input('Informe o filme que deseja alugar: '))
+        aluga_filmes.aluga_filme(aluga)
+    case 3:
+        devolve = int(input('Informe o filme que deseja devolver: '))
+        aluga_filmes.devolve_filme(devolve)
